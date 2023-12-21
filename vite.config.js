0d8dce1118187376f6ffc3dec6416037d00c0323
Jose Daniel Anacona <jdanaconar@gmail.com>
2023-12-21 14:41:28 -0500

    Vegetalien - demostacion 0.1

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "https://github.com/JDDAR/Vegetalien/",
+  base: "https://jddar.github.io/Vegetalien",
 })
