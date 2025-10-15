f9d712e98e4f15bccc085baa6bf94d831655839b
JDAAR <jdanaconar@gmail.com>
2025-10-14 20:36:45 -0500

    Actualizando estilos para el menú publico, se agregan header

---
+++ src/components/userPublic/headerPublicMenu/HeaderMenuPublic.jsx
@@ -0,0 +1,14 @@
+import "./headerMenuPublic.scss"; 
+const HeaderMenuPublic = () => {   
+  return( 
+    <>
+      <div className="header-container">
+        <div className="header-container-logo">
+          <h2>Le Végétalien</h2>
+          <span>au Pola</span>
+        </div> 
+      </div>
+    </> 
+  )
+}
+export default HeaderMenuPublic;
